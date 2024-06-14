ce3bf8d10be191b75b007a24bec37b3af03f2bb3
Aman Sinha <73378135+tranquillite-007@users.noreply.github.com>
2024-06-14 23:54:21 +0530

    Delete myauth directory

--- myauth/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('register/', views.register_view, name='register'),
-]
