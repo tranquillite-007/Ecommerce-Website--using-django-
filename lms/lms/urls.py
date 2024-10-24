15c7bdae517f43d7719c419f62a84dc62fa49de5
Aman Sinha <73378135+tranquillite-007@users.noreply.github.com>
2024-10-25 04:52:03 +0530

    added lms (library management system)

---
+++ lms/lms/urls.py
@@ -0,0 +1,10 @@
+# lms/urls.py
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import RedirectView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('', RedirectView.as_view(url='/accounts/login/', permanent=True)),  # Redirect root to login
+]
