700da16213146569a19121c24c832197eaf5df73
tranquillite-007 <sinhaaman655@gmail.com>
2024-12-11 12:11:32 +0530

    lms removed 11-12-24

--- liberary_management_system/accounts/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length = 150)
-    password = forms.CharField(widget=forms.PasswordInput)
-    user_type = forms.ChoiceField(choices=[
-        ('user', 'User'),
-        ('admin', 'Admin')
-    ])
